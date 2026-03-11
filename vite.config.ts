b701d181fd037d3519fa0cdf71534cd7907f2157
mrSaT13 <102960998+mrSaT13@users.noreply.github.com>
2026-03-11 16:26:44 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import { createManualChunks } from './src/manual-chunks'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      cy: path.resolve(__dirname, './cypress'),
+    },
+  },
+  build: {
+    minify: 'terser',
+    rollupOptions: {
+      external: ['bufferutil', 'utf-8-validate'],
+      output: {
+        manualChunks: createManualChunks,
+      },
+    },
+  },
+})
